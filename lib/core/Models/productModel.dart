23d8b0f453712b4ad0dba5b06218e290411a2636
Lena balakumar Subbarayan <lenabalakumar@gmail.com>
2021-05-31 15:31:27 +0530

    May 31, 2021. Commit 5.

--- lib/core/Models/productModel.dart
+++ lib/core/Models/productModel.dart
@@ -1,15 +1,29 @@
 class ProductModel{
   String productName;
-  int productID;
   double productPrice;
-  int productQuantity;
+  String productWeight;
   String productImageURL;
 
   ProductModel({
     this.productName,
-    this.productID,
     this.productPrice,
-    this.productQuantity,
+    this.productWeight,
     this.productImageURL
 });
 }
+
+var topProduct1 = ProductModel(productName: 'Country tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct2 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct3 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct4 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct5 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct6 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct7 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct8 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct9 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct10 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct11 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+var topProduct12 = ProductModel(productName: 'Tomato', productPrice: 10.0, productWeight: '1 kg', productImageURL: 'assets/images/tomato.jpg');
+
+
+var tProductData = [topProduct1, topProduct2, topProduct3, topProduct4, topProduct5, topProduct6, topProduct7, topProduct8, topProduct9, topProduct10, topProduct11, topProduct12];
